4396c3ff0cbe0d0fc0ec4505981e13fb596eb3e9
chrisIyer <hellohotbox420@gmail.com>
2023-08-30 12:50:04 -0400

    not sure where I am on the front end, but I'm somewhere.

---
+++ src/fetchSearch.js
@@ -0,0 +1,17 @@
+async function fetchBreedList({ queryKey }) {
+     const animal = queryKey[1];
+   
+     if (!animal) return [];
+   
+     const res = await fetch(
+       `http://localhost:3008/api/item/breeds?animal=${animal}`
+     );
+   
+     if (!res.ok) {
+       throw new Error(`breeds ${animal} fetch not ok`);
+     }
+   
+     return res.json();
+   }
+   
+   export default fetchBreedList;
